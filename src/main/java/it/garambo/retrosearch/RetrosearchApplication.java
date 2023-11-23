7631171e0f86a05fca0c33436df54651d6218dd5
Vincenzo Garambone <garambo@gmail.com>
2023-11-23 10:29:02 +0100

    Init: project setup

---
+++ src/main/java/it/garambo/retrosearch/RetrosearchApplication.java
@@ -0,0 +1,13 @@
+package it.garambo.retrosearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetrosearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetrosearchApplication.class, args);
+	}
+
+}
