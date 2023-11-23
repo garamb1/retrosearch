92a0fc7877140cc99c2720e617a7e143f1aee4bf
Vincenzo Garambone <garambo@gmail.com>
2023-11-23 10:29:02 +0100

    Init: project setup

---
+++ src/test/java/it/garambo/retrosearch/RetrosearchApplicationTests.java
@@ -0,0 +1,13 @@
+package it.garambo.retrosearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetrosearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
