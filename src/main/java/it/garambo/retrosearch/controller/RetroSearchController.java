403f34fc0afbb3450dfc2e4479cd5a0430a40a94
Vincenzo Garambone <garambo@gmail.com>
2023-11-25 11:35:48 +0100

    UI Improvements
    UI was improved by dividing it in fragments, loaded by thymeleaf.
    Spring Controllers were streamlined to a single one.
    Readme updated

--- src/main/java/it/garambo/retrosearch/controller/RetroSearchController.java
+++ src/main/java/it/garambo/retrosearch/controller/RetroSearchController.java
@@ -2,7 +2,10 @@ package it.garambo.retrosearch.controller;
 
 import it.garambo.retrosearch.browse.model.ParsedHtmlPage;
 import it.garambo.retrosearch.browse.parser.ModernHtmlParser;
+import it.garambo.retrosearch.ddg.client.DDGClient;
+import it.garambo.retrosearch.ddg.model.SearchResults;
 import java.net.URI;
+import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.validator.routines.UrlValidator;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,11 +16,35 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Slf4j
 @Controller
-public class WebBrowsingController {
+public class RetroSearchController {
 
+  @Autowired private DDGClient ddgClient;
   @Autowired private UrlValidator urlValidator;
-
   @Autowired private ModernHtmlParser htmlParser;
+
+  @GetMapping("/search")
+  public String search(
+      @RequestParam() Optional<String> query, @RequestParam Optional<String> locale, Model model) {
+    if (query.isEmpty() || query.get().isEmpty()) {
+      return "redirect:/";
+    }
+
+    try {
+      SearchResults searchResults =
+          locale.isEmpty()
+              ? ddgClient.search(query.get())
+              : ddgClient.search(query.get(), locale.get());
+      model.addAttribute(searchResults);
+      return "search-results";
+    } catch (Exception e) {
+      log.error(
+          "Error getting search result, for parameter query: {}, locale: {}", query, locale, e);
+      model.addAttribute("error", "Could not get search results. Sorry :(");
+      model.addAttribute(
+          "details", "Your search query '" + query.get() + "' could not be processed");
+      return "error-details";
+    }
+  }
 
   @GetMapping("/browse")
   public String browse(@RequestParam String url, Model model) {
@@ -37,5 +64,10 @@ public class WebBrowsingController {
       model.addAttribute("details", "Original address: " + uri);
       return "error-details";
     }
+  }
+
+  @GetMapping("/about")
+  public String about() {
+    return "about";
   }
 }
