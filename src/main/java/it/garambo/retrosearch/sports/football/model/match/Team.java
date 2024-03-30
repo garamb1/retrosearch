0f5501d5bef4ec33555938e147a923b213f58d9a
Vincenzo Garambone <garambo@gmail.com>
2024-03-30 18:49:15 +0100

    refactor, add tests

--- src/main/java/it/garambo/retrosearch/sports/football/model/match/Team.java
+++ src/main/java/it/garambo/retrosearch/sports/football/model/match/Team.java
@@ -1,3 +1,6 @@
 package it.garambo.retrosearch.sports.football.model.match;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record Team(int id, String name, String shortName) {}
--- src/main/java/it/garambo/retrosearch/sports/football/repository/FootballDataRepository.java
+++ src/main/java/it/garambo/retrosearch/sports/football/repository/FootballDataRepository.java
@@ -2,13 +2,14 @@ package it.garambo.retrosearch.sports.football.repository;
 
 import it.garambo.retrosearch.sports.football.model.match.Match;
 import java.util.Date;
+import java.util.List;
 import java.util.Set;
 
-public interface SportsRepository {
+public interface FootballDataRepository {
 
   Set<Match> getAllMatchesByArea(String areaName);
 
-  void updateAll(Set<Match> newMatches);
+  void updateAll(List<Match> newMatches);
 
   Date getUpdatedAt();
 }
