fc9d5aaba386482508b97f2b5f7e30912f109ccb
Vincenzo Garambone <garambo@gmail.com>
2024-04-04 10:37:01 +0200

    Football scores results by date range

--- src/main/java/it/garambo/retrosearch/sports/football/client/FootballDataOrgClient.java
+++ src/main/java/it/garambo/retrosearch/sports/football/client/FootballDataOrgClient.java
@@ -1,12 +1,18 @@
 package it.garambo.retrosearch.sports.football.client;
 
+import static java.util.Objects.isNull;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import it.garambo.retrosearch.http.HttpService;
 import it.garambo.retrosearch.sports.football.model.FootballDataResponse;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import org.apache.http.message.BasicHeader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -17,18 +23,27 @@ import org.springframework.stereotype.Component;
 @ConditionalOnProperty(value = "retrosearch.sports.football.enable", havingValue = "true")
 public class FootballDataOrgClient {
 
-  private final String API_URL = "https://api.football-data.org/v4/matches/";
+  private final String API_URL = "https://api.football-data.org/v4/matches";
 
   @Value("${retrosearch.sports.football.api.key:}")
   private String apiKey;
 
   @Autowired HttpService httpService;
 
-  public FootballDataResponse fetchFootballData() throws IOException, URISyntaxException {
+  public FootballDataResponse fetchFootballData(LocalDate dateFrom, LocalDate dateTo)
+      throws IOException, URISyntaxException {
     URI apiUri = new URI(API_URL);
     BasicHeader apiKeyHeader = new BasicHeader("X-Auth-Token", apiKey);
 
-    String response = httpService.get(apiUri, List.of(apiKeyHeader));
+    Map<String, String> params = new HashMap<>();
+    if (!isNull(dateFrom) && !isNull(dateTo)) {
+      String formattedDateFrom = dateFrom.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+      String formattedDateTo = dateTo.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+      params.put("dateFrom", formattedDateFrom);
+      params.put("dateTo", formattedDateTo);
+    }
+
+    String response = httpService.get(apiUri, params, List.of(apiKeyHeader));
     return new ObjectMapper().readValue(response, FootballDataResponse.class);
   }
 }
--- src/main/java/it/garambo/retrosearch/sports/football/scheduled/FootballDataScheduledTask.java
+++ src/main/java/it/garambo/retrosearch/sports/football/scheduled/FootballDataScheduledTask.java
@@ -1,10 +1,14 @@
 package it.garambo.retrosearch.sports.football.scheduled;
 
+import static java.util.Objects.isNull;
+
 import it.garambo.retrosearch.sports.football.client.FootballDataOrgClient;
 import it.garambo.retrosearch.sports.football.model.FootballDataResponse;
 import it.garambo.retrosearch.sports.football.repository.FootballRepository;
+import java.time.LocalDate;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -18,11 +22,20 @@ public class FootballDataScheduledTask {
 
   @Autowired private FootballRepository repository;
 
+  @Value("${retrosearch.sports.football.results.past.days:3}")
+  private Integer pastDays;
+
   @Scheduled(fixedRate = 30 * 60 * 1000)
   private void updateFootballData() {
+    LocalDate dateTo = LocalDate.now();
+    LocalDate dateFrom = null;
+    if (!isNull(pastDays) && pastDays > 0) {
+      dateFrom = dateTo.minusDays(pastDays);
+    }
+
     try {
-      log.info("Updating football result list...");
-      FootballDataResponse footballData = apiClient.fetchFootballData();
+      log.info("Updating football result list from {} to {}...", dateFrom, dateTo);
+      FootballDataResponse footballData = apiClient.fetchFootballData(dateFrom, dateTo);
       repository.updateAll(footballData.matches());
     } catch (Exception e) {
       log.error("Football result list update failed:", e);
