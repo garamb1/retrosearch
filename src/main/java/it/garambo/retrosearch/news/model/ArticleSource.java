a18317f89293973af4d52ec253167ab7c7330c54
Vincenzo Garambone <garambo@gmail.com>
2025-10-03 23:28:52 +0200

    ignore unknown properties for Gnews client

--- src/main/java/it/garambo/retrosearch/news/model/ArticleSource.java
+++ src/main/java/it/garambo/retrosearch/news/model/ArticleSource.java
@@ -1,3 +1,6 @@
 package it.garambo.retrosearch.news.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record ArticleSource(String name, String url) {}
--- src/main/java/it/garambo/retrosearch/news/model/GNewsApiResponse.java
+++ src/main/java/it/garambo/retrosearch/news/model/GNewsApiResponse.java
@@ -1,5 +1,8 @@
 package it.garambo.retrosearch.news.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record GNewsApiResponse(int totalArticles, List<Article> articles) {}
