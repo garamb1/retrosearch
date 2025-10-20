e1990fdfd3a4d97cc43b6239aa272e2b2002c8cb
Vincenzo Garambone <garambo@gmail.com>
2025-10-20 09:23:04 +0200

    Improve rate limiting

--- src/main/java/it/garambo/retrosearch/controller/RateLimitingFilter.java
+++ src/main/java/it/garambo/retrosearch/controller/RateLimitingFilter.java
@@ -1,34 +1,46 @@
 package it.garambo.retrosearch.controller;
 
 import io.github.bucket4j.Bucket;
-import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.http.HttpStatus;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 @Slf4j
 @RequiredArgsConstructor
-public class RateLimitingFilter implements Filter {
+public class RateLimitingFilter extends OncePerRequestFilter {
 
   private final Bucket rateLimitingBucket;
+  private final List<String> excludedPaths;
+  private final AntPathMatcher pathMatcher = new AntPathMatcher();
 
   @Override
-  public void doFilter(
-      ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
-      throws IOException, ServletException {
+  protected void doFilterInternal(
+      @NotNull HttpServletRequest request,
+      @NotNull HttpServletResponse response,
+      FilterChain filterChain)
+      throws ServletException, IOException {
     if (rateLimitingBucket.tryConsume(1)) {
-      filterChain.doFilter(servletRequest, servletResponse);
+      filterChain.doFilter(request, response);
+      return;
     }
 
-    log.info("Too many requests!");
-    HttpServletResponse httpResponse = (HttpServletResponse) servletResponse;
-    httpResponse.setStatus(HttpStatus.TOO_MANY_REQUESTS.value());
-    httpResponse.getWriter().write("Too many requests.");
+    log.warn("Too many requests!");
+    response.setStatus(HttpStatus.TOO_MANY_REQUESTS.value());
+    response.getWriter().write("Too many requests.");
+  }
+
+  @Override
+  protected boolean shouldNotFilter(@NotNull HttpServletRequest request) {
+    return excludedPaths.stream()
+        .anyMatch(e -> pathMatcher.match(e, request.getServletPath()));
   }
 }
